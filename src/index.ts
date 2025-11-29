16b4b73fb9038bf66ad2ca0c6ffd484597d74abb
User <tazik1232@gmail.com>
2025-11-29 21:47:21 +0200

    Add TypeScript config and example code

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const username: string = "Тарас";
+const age: number = 25;
+const isActive: boolean = true;
+
+console.log(`Користувач: ${username}`);
+console.log(`Вік: ${age}`);
+console.log(`Активний: ${isActive}`);
